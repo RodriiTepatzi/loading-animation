7835088e9767b75efe3bcf81d20f4a53bceb3be7
RodriiTepatzi <rodrif19@hotmail.com>
2024-04-10 13:53:12 -0600

    init

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { StyleSheet, SafeAreaView, Animated, Text } from 'react-native';
 
 export default function App() {
+  const rotation = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(rotation, {
+        toValue: 1,
+        duration: 2000,
+        useNativeDriver: false,
+      })
+    ).start();
+  }, []);
+
+  const spin = rotation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Animated.View style={[styles.circle, { transform: [{ rotate: spin }] }]} />
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +32,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  circle: {
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+    borderWidth: 5,
+    borderColor: '#b3a2ff',
+    borderStyle: 'solid',
+    borderTopColor: 'transparent',
+    margin: 20
   },
 });
